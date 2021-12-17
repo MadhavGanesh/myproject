4b68aa200463b252f5199c1e2b8ada8a316b61bd
MadhavGanesh <gmadhavganesh@gmail.com>
2021-12-17 19:17:08 +0530

    my second Commit

---
+++ js/samplejs.js
@@ -0,0 +1,48 @@
+
+function Addition(a,b){
+	return a*b;
+}
+
+function Addition(a,b){
+	return a+b;
+}
+
+function Multiply(a,b){
+	return a*b;
+}
+
+console.log("a=4,b=5")
+console.log("Addition(a+b): "+Addition(4,5))
+
+
+var x="ganesh";
+x=10;
+x=false;
+
+let y="hi";
+let y=10;
+
+
+console.log(x);
+console.log(y);
+
+var names=[1,2,3,4];
+typeof(names);
+console.log(names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
